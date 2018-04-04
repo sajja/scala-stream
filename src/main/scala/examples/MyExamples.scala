fe0d3cb9ad4e82ce135b6f771917338dffab8814
Sajith Silva <sajiths@pagero.com>
2018-04-04 10:16:40 +0530

    Akka streaming with kafka

--- src/main/scala/examples/MyExamples.scala
+++ src/main/scala/examples/MyExamples.scala
@@ -2,12 +2,16 @@ package examples
 
 import java.nio.file.Paths
 
-import akka.NotUsed
+import akka.{Done, NotUsed}
 import akka.actor.{ActorSystem, Props}
+import akka.kafka.{ConsumerSettings, Subscriptions}
+import akka.kafka.scaladsl.Consumer
 import akka.stream._
 import akka.stream.actor.ActorPublisher
 import akka.stream.scaladsl.{Broadcast, FileIO, Flow, GraphDSL, Keep, Merge, Partition, RunnableGraph, Sink, Source}
 import akka.util.ByteString
+import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.common.serialization.{ByteArrayDeserializer, StringDeserializer}
 import org.reactivestreams.{Subscriber, Subscription}
 
 import scala.collection
@@ -30,6 +34,27 @@ object MyExamples {
 
   case class Event(et: String, ad: String, value: Int)
 
+  def t0() = {
+    val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
+      val in = Source.fromIterator(() => collection.Iterator.continually[Int] {
+        Thread.sleep(1000)
+        Random.nextInt(10)
+      })
+      val out = Sink.foreach(println)
+
+      val batcher = Flow[Int].map(x => s"$x").batch(6000, a => a + "VVV")((agg, elem) => {
+        println(agg +"ZZZ")
+        val i=0
+        if (i == 0) throw new Exception("XXx")
+        agg + elem
+      })
+      in ~> batcher ~> out
+
+      ClosedShape
+    }).run()
+  }
+
 
   def t1() = {
     val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
@@ -48,7 +73,9 @@ object MyExamples {
     }).run()
   }
 
-  case class Doc(id: Int, endpoint: String)
+  case class Doc(id: Int, endpoint: String) {
+    override def toString() = s"$id:$endpoint"
+  }
 
   def generateDoc() = {
     val allEndPoints = List("21G", "BASWARE", "1stBP", "HansaPrint")
@@ -72,6 +99,68 @@ object MyExamples {
     }).run()
   }
 
+
+  def delivery3(): Unit = {
+    val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
+      val consumerSettings: ConsumerSettings[Array[Byte], String] = ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
+        .withBootstrapServers("localhost:9092")
+        .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "none")
+        .withProperty(ConsumerConfig.GROUP_ID_CONFIG, "100")
+        .withProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+      val input = Consumer.plainSource[Array[Byte], String](consumerSettings, Subscriptions.topics("test"))
+      val s: Sink[Any, Future[Done]] = Sink.foreach {
+        case c: ConsumerRecord[Array[Byte], String] => println(c.value())
+        case i => println(i)
+      }
+
+      input ~> s
+      ClosedShape
+    }).run()
+  }
+
+  def delivery4(): Unit = {
+    val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
+      import GraphDSL.Implicits._
+
+      val consumerSettings: ConsumerSettings[Array[Byte], String] = ConsumerSettings(system, new ByteArrayDeserializer, new StringDeserializer)
+        .withBootstrapServers("localhost:9092")
+        .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "none")
+        .withProperty(ConsumerConfig.GROUP_ID_CONFIG, "100")
+        .withProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true")
+
+      val input = Consumer.plainSource[Array[Byte], String](consumerSettings, Subscriptions.topics("test"))
+
+
+      val endpoint = builder.add(Partition[Object](5, {
+        case cr: ConsumerRecord[Array[Byte], String] if cr.value().contains("21G") => 0
+        case cr: ConsumerRecord[Array[Byte], String] if cr.value().contains("BASWARE") => 1
+        case cr: ConsumerRecord[Array[Byte], String] if cr.value().contains("1stBP") => 2
+        case cr: ConsumerRecord[Array[Byte], String] if cr.value().contains("HansaPrint") => 3
+        case _ => 4
+      }))
+
+      val twgBatcher = Flow[Object].map(x => x).grouped(10)
+      val baswareBatcher = Flow[Object].map(x => x).batch(5000, a => a)((a, b) => s"$a+$b")
+
+      input ~> endpoint.in
+
+      endpoint.out(0) ~> twgBatcher ~> createSink("21G")
+      endpoint.out(1) ~> baswareBatcher ~> createSink("Basware")
+      endpoint.out(2) ~> createSink("1stBP")
+      endpoint.out(3) ~> createSink("HansaPrint")
+      endpoint.out(4) ~> createSink("Default")
+
+      ClosedShape
+    }).run()
+  }
+
+  def createSink(name: String): Sink[Any, Future[Done]] = Sink.foreach {
+    case c: ConsumerRecord[Array[Byte], String] => println(s"$name : ${c.value()}")
+    case i => println(i)
+  }
+
+
   def delivery(): Unit = {
     val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
       import GraphDSL.Implicits._
@@ -86,6 +175,7 @@ object MyExamples {
       val other = Sink.foreach[Doc](x => println(s"Other steam $x"))
 
       val batcher = Flow[Doc].map(x => x).grouped(30)
+      //      val batcher2 = Flow[Doc].map(x => x).batch(1000,a=>a)
 
       val endpoint = builder.add(Partition[Doc](5, doc => doc.endpoint match {
         case "21G" =>
@@ -202,7 +292,7 @@ object MyExamples {
   }
 
   def main(args: Array[String]): Unit = {
-    delivery()
+    delivery4()
   }
 
   def ccTransactions() = Source.fromIterator(() => Iterator.continually((Random.nextInt(10), Random.nextInt(500))))
--- src/main/scala/kafka/TestProducer.scala
+++ src/main/scala/kafka/TestProducer.scala
@@ -6,13 +6,15 @@ import akka.kafka.scaladsl.Producer
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Source
 import com.typesafe.config.{Config, ConfigFactory}
+import examples.MyExamples.Doc
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.{ByteArraySerializer, StringSerializer}
 
 import scala.util.Random
 
 
-object TestProducer1 {
+object TestKafkaProducer {
   val data = List("Sajith:Hello", "Silva:World")
   val r = Random
 
@@ -21,6 +23,11 @@ object TestProducer1 {
   }
 
   val config: Config = ConfigFactory.load()
+
+  def generateDoc() = {
+    val allEndPoints = List("21G", "BASWARE", "1stBP", "HansaPrint")
+    Doc(Random.nextInt(1000), allEndPoints(Random.nextInt(3)))
+  }
 
   def main(args: Array[String]): Unit = {
 
@@ -31,15 +38,17 @@ object TestProducer1 {
 
 
     val producerSettings = ProducerSettings(system, new ByteArraySerializer, new StringSerializer)
-      .withBootstrapServers("localhost:9092")
+      .withBootstrapServers("localhost:9092").withProperty(ConsumerConfig.GROUP_ID_CONFIG,"100")
     val r = Random
 
     val done = Source(1 to Int.MaxValue)
       .map(_.toString)
       .map { elem =>
         Thread.sleep(1000)
-        new ProducerRecord[Array[Byte], String]("test", data(r.nextInt(2)))
+        val data = generateDoc().toString()
+        println(data)
+        new ProducerRecord[Array[Byte], String]("test", data)
       }
-      .runWith(Producer.plainSink(producerSettings))
+      .runWith(Producer.plainSink[Array[Byte], String](producerSettings))
   }
 }
